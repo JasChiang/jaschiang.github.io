c7d2994911386e4c3e06ddaa54adf9382900b88f
JasChiang <jaschiang@live.com>
2024-12-16 11:44:08 +0800

    second commit

--- network-landing/vite.config.ts
+++ network-landing/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/network-landing/',  // 加入這行，注意要和倉庫名稱一樣
   plugins: [react()],
-  base: '/network-landing/' // 改成你想要的資料夾名稱
 })
